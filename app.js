69b2f0d01d9b30423c437e920913f4d0673980d6
Ivan Savchenko <ivan.savchenko@eleks.com>
2017-03-07 18:49:07 +0200

    basic security

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const port = process.env.PORT || 8000;
 const app = express();
 const server = app.listen(port, () => console.log('Listening on port %d', server.address().port));
 
+app.use('*', function(req, res, next) {
+  const user = process.env.API_USER;
+  const password = process.env.API_PASSWORD;
+  const securityToken = `${user}_${password}`;
+  const authToken = req.headers.authorization;
+
+  if (!authToken || authToken !== securityToken) {
+    return res.send(401);
+  }
+
+  return next();
+});
+
 app.get('*', (req, res) => {
   res.send('I\'m alive');
 });
