914627c4ab5feac37d3a9d67873cd2f5aaa582ae
Ivan Savchenko <ivan.savchenko@eleks.com>
2017-03-13 18:57:53 +0200

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,23 @@
-module.exports = function(server) {
-  server.post('/login', () => res.send('login'));
+const jwt    = require('jsonwebtoken');
+
+module.exports = function(server, models) {
+  server.post('/login', (req, res, next) => {
+    models.users.get(req.body.user)
+      .then(user => {
+        if (user.password !== req.body.password) {
+          return next({status: 401, message: 'Bad credentials'});
+        }
+
+        const token = jwt.sign(user, process.env.SECRET, {
+          expiresInMinutes: 1440
+        });
+
+        res.send({
+          token, token
+        });
+      })
+      .catch(error => next(error));
+  });
+
   server.get('/logout', () => res.send('logout'));
 };
